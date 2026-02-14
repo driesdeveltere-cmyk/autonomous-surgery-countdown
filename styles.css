0c5fc0a7dad962d7b1689b243653b4479374809e
driesdeveltere-cmyk <dries_develtere@hotmail.com>
2026-02-14 21:06:00 +0100

    Add initial styles for body and headings
    Added basic styles for body, h1, and p elements.

--- styles.css
+++ styles.css
@@ -1,1 +1,16 @@
+body {
+  background-color: #0b0f14;
+  color: #e8eef6;
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  padding-top: 80px;
+}
+
+h1 {
+  font-size: 42px;
+}
+
+p {
+  color: #aab6c5;
+}
 
